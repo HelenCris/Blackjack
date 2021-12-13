bb54f9953711d6519cd08bc11bd3bed2b7bc35e8
HelenCris <79730539+HelenCris@users.noreply.github.com>
2021-12-13 17:39:38 -0300

    projeto do dia 12 realizado com sucesso

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,51 +4,37 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
 the_number = random.randint(1, 100)
-choose_difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+choose_difficulty = int(input("Choose a difficulty.\n1 - Easy\n2 - Hard \nType:  "))
 
-if choose_difficulty == "easy": 
-    print("You have 10 attempts remain  to guess the number.")
-    number_attempts = 10
-    play =  False
-    while not play:
-        number_attempts -= 1
-        guess = int(input("Make a guess: "))
-        if guess == the_number:
-            print(f"You got it! The answer was {the_number}.")
-            play = True 
-        elif guess > the_number:
-            print("Too high")
+
+def level():
+    if choose_difficulty == 1: 
+       return 10     
+    elif choose_difficulty == 2: 
+        return 5 
+    else:
+        print("You write wrong! Try again.")
+
+class Sorting:
+    def game(turn):
+        turn = level()
+        print(f"You have {turn} attempts remain  to guess the number.") 
+        for i in range(turn):
+            guess = int(input("Make a guess: "))
+            if ((turn - i) -1) == 0:
+                print("You've run out of guesses, you lose.")
+                break
+            else:
+                if guess == the_number:
+                    print(f"You got it! The answer was {the_number}.")
+                    break
+                elif guess > the_number:
+                    print("Too high")
+                elif guess < the_number:
+                    print("Too low") 
             print("Guess again.")
-            print(f"You have {number_attempts} attempts remaining to guess the number. ")
-        elif guess < the_number:
-            print("Too low")
-            print("Guess again.")
-            print(f"You have {number_attempts} attempts remaining to guess the number. ")
-        elif number_attempts == 0:
-            print("You've run out of guesses, you lose.")
-            break
-        continue
-        
-if choose_difficulty == "hard": 
-    print("You have 5 attempts remain  to guess the number.")
-    number_attempts = 5
-    play =  False
-    while not play:
-        guess = int(input("Make a guess: "))
-        if guess == the_number:
-            print(f"You got it! The answer was {the_number}.")
-            play = True 
-        elif guess > the_number:
-            print("Too high")
-            print("Guess again.")
-            number_attempts -= 1
-            print(f"You have {number_attempts} attempts remaining to guess the number. ")
-        elif guess < the_number:
-            print("Too low")
-            print("Guess again.")
-            print(f"You have {number_attempts} attempts remaining to guess the number. ")
-        elif number_attempts == 0:
-            print("You've run out of guesses, you lose.")
-            play = True
-        continue
-        
+            print(f"You have {(turn - i) - 1} attempts remain  to guess the number.") 
+       
+Sort = Sorting()
+Sort.game()
+
